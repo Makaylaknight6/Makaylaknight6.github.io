43b239202645039dee8d06a11b4e73a66dccaa6b
Makaylaknight6 <makayla.knight1@jpschools.org>
2025-03-13 20:42:40 +0000

    Finalizedplatformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -17,7 +17,7 @@ $(function () {
     }
 
     // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
+    createPlatform(150, -50, -50, -50 
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
@@ -31,21 +31,29 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
+createPlatform(150, 650, 50, 50, "red");
+createPlatform(250, 575, 50, 50, "purple")
+createPlatform(350, 500, 50, 50, "yellow")
+createPlatform(450, 650, 50, 50, "green")
+createPlatform(650, 550, 200, 50, "pink"
 
-
-
+)
+    
 
     // TODO 3 - Create Collectables
-
-
-
+createCollectable("grace", 350, 450, 1.0, 1.0);
+createCollectable("steve", 200, 349, 1.0, 1.0)
+createCollectable("max", 500, 200, 1.0, 1.0)
     
-    // TODO 4 - Create Cannons
+  
+
+ // TODO 4 - Create Cannons
+createCannon("right", 550, 2000);
+createCannon("left", 400, 1000)
+createCannon("bottom", 550, 1000)
 
 
-    
-    
-    //////////////////////////////////
+//////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
   }
